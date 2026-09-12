0916982cfab15e43ef42f5c3b70a4b5493bc7df6
Reskilling <64717632+Reskilling@users.noreply.github.com>
2026-09-11 20:46:38 -0700

    Add Nostalgia Rewind addon logo

--- manifest.js
+++ manifest.js
@@ -18,10 +18,11 @@ const enabledCatalogs = catalogs
 
 module.exports = {
   id: 'community.nostalgiarewind.catalogs',
-  version: '1.0.0',
+  version: '1.0.1',
   name: 'Nostalgia Rewind',
   description:
     'A curated nostalgia collection of TV series and movies from the 1990s and early 2000s.',
+  logo: 'https://2f680d7c8e22-nostalgia-rewind.baby-beamup.club/public/Nostalgia-Rewind.png',
   resources: [
     {
       name: 'catalog',
